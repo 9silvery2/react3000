da1f2109b746bde50e71e55363e0b8a6b5932413
9silvery2 <80299197+9silvery2@users.noreply.github.com>
2021-05-25 13:18:52 +0900

    2021.05.25

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props){
-    return <h1>Hello, {props.name}</h1>;
+function formatDate(date){
+    return date.toLocaleDateString();
 }
 
-function App(){
+function Comment(props){
     return (
-        <div>
-            <Welcome name="sil" />
-            <Welcome name="ver" />
-            <Welcome name="y" />
+        <div className="comment">
+            <div className="user-info">
+                <img className="avatar" src={props.author.avatarUrl} alt={props.author.name} />
+                <div className="user-name">{props.author.name}</div>
+            </div>
+            <div className="comment-text">{props.text}</div>
+            <div className="comment-date">{formatDate(props.date)}</div>
         </div>
-    );
+    )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const comment = {
+    date: new Date(),
+    text: "갑작스럽게 떠나는 여행을 좋아한다.",
+    author: {
+        name: "silvery",
+        avatarUrl:
+        "http://silvery92.dothome.co.kr/portfolio/assets/images/aboutImg01.jpg"
+    },
+};
+
+ReactDOM.render(
+    <Comment date={comment.date} text={comment.text} author={comment.author} />
+    , document.getElementById('root'));
