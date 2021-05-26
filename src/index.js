e273dc1f030c2ed681ea0e73fb442d6aff33cde4
9silvery2 <80299197+9silvery2@users.noreply.github.com>
2021-05-26 13:18:07 +0900

    2021.05.26.pagemake

--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App";
+import "./index.scss";
 
-function formatDate(date){
-    return date.toLocaleDateString();
-}
-
-function Avatar(props){
-    return (
-        <img src={props.user.avatarUrl} alt={props.user.name} />
-    )
-}
-
-function UserInfo(props){
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div>{props.user.name}</div>
-        </div>
-    )
-}
-
-function Comment(props){
-    return (
-        <div className="Comment">
-            <UserInfo user={props.author} />
-            <div className="Comment-text">{props.text}</div>
-            <div className="Comment-date">{formatDate(props.date)}</div>
-        </div>
-    )
-}
-
-const comment = {
-    date: new Date(),
-    text: "갑작스럽게 떠나는 여행을 좋아한다.",
-    author: {
-        name: "silvery",
-        avatarUrl:
-        "http://silvery92.dothome.co.kr/portfolio/assets/images/aboutImg01.jpg"
-    },
-};
-
-ReactDOM.render(<Comment date={comment.date} text={comment.text} author={comment.author} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
